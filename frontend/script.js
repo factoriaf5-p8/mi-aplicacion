abc5ebf7454a4f55e44ba21ee3a637835babfd61
Raul <you@example.com>
2023-12-04 11:09:49 +0100

    add async-await function fetch

--- frontend/script.js
+++ frontend/script.js
@@ -1,20 +1,45 @@
-fetch("http://localhost:3001/products")
-    .then((response)=>{
-        return response.json();
-    })
-    .then((data)=>{
-        console.log(data);
+// fetch("http://localhost:3001/products")
+//     .then((response)=>{
+//         return response.json();
+//     })
+//     .then((data)=>{
+//         console.log(data);
+//         const gallery = document.querySelector('.gallery');
+//         const product = document.createElement('article');
+//         const title = document.createElement('h1');
+//         const img = document.createElement('img');
+
+//         data.forEach(element =>{
+//         img.src = element.image;
+//         title.innerText=element.name;
+//         product.appendChild(title);
+//         product.appendChild(img);
+//         gallery.appendChild(product);})
+//     })
+//     .catch(error => document.write(error))
+// console.log(result);
+
+const fetchProduct = async () =>{
+    try {
+
+        const result = await fetch("http://localhost:3001/products");
+        const data = await result.json();
         const gallery = document.querySelector('.gallery');
+
         const product = document.createElement('article');
         const title = document.createElement('h1');
         const img = document.createElement('img');
 
         data.forEach(element =>{
-        img.src = element.image;
-        title.innerText=element.name;
-        product.appendChild(title);
-        product.appendChild(img);
-        gallery.appendChild(product);})
-    })
-    .catch(error => document.write(error))
-// console.log(result);
+            img.src = element.image;
+            title.innerText=element.name;
+            product.appendChild(title);
+            product.appendChild(img);
+            gallery.appendChild(product);})
+
+   } catch (error) {
+        document.write(error);
+    }
+}
+
+fetchProduct();
